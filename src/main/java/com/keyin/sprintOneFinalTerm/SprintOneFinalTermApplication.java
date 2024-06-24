73e4d0611919f0cfe609a3d2da6120766551142d
Brandon Butler <85582593+BrandonButler123@users.noreply.github.com>
2024-06-24 20:33:04 -0230

    Initial commit

---
+++ src/main/java/com/keyin/sprintOneFinalTerm/SprintOneFinalTermApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.sprintOneFinalTerm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintOneFinalTermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintOneFinalTermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyin/sprintOneFinalTerm/SprintOneFinalTermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.sprintOneFinalTerm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintOneFinalTermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
