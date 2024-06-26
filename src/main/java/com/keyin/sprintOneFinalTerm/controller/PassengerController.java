d47afeeed44b2f71a0a4fc08e96864284dc727d8
Brandon Butler <85582593+BrandonButler123@users.noreply.github.com>
2024-06-26 15:53:58 -0230

    updates
    updates

--- src/main/java/com/keyin/sprintOneFinalTerm/controller/PassengerController.java
+++ src/main/java/com/keyin/sprintOneFinalTerm/controller/PassengerController.java
@@ -1,7 +1,9 @@
 package com.keyin.sprintOneFinalTerm.controller;
 
+import com.keyin.sprintOneFinalTerm.model.Aircraft;
 import com.keyin.sprintOneFinalTerm.model.Passenger;
-import com.keyin.sprintOneFinalTerm.service.PassengerService
+import com.keyin.sprintOneFinalTerm.model.City;
+import com.keyin.sprintOneFinalTerm.service.PassengerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,5 +34,15 @@ public class PassengerController {
     @GetMapping
     public List<Passenger> getAllPassengers() {
         return passengerService.getAllPassengers();
+    }
+
+    @PostMapping("/{passengerId}/aircraft")
+    public Passenger addAircraftsToPassenger(@PathVariable int passengerId, @RequestBody Aircraft aircraft) {
+        return passengerService.addAircraftsToPassenger(passengerId, aircraft);
+    }
+
+    @PostMapping("/{passengerId}/city")
+    public Passenger addCityToPassenger(@PathVariable int passengerId, @RequestBody City city) {
+        return passengerService.addCityToPassenger(passengerId, city);
     }
 }
