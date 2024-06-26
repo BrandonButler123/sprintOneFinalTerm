eaa45db284b449fe0a2d6136c4883b83e0dc9ee5
Brandon Butler <85582593+BrandonButler123@users.noreply.github.com>
2024-06-26 13:43:55 -0230

    update PassengerController.java

--- src/main/java/com/keyin/sprintOneFinalTerm/controller/PassengerController.java
+++ src/main/java/com/keyin/sprintOneFinalTerm/controller/PassengerController.java
@@ -1,4 +1,36 @@
 package com.keyin.sprintOneFinalTerm.controller;
 
+import com.keyin.sprintOneFinalTerm.model.Passenger;
+import com.keyin.sprintOneFinalTerm.service.PassengerService
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/passengers")
 public class PassengerController {
+
+    @Autowired
+    private PassengerService passengerService;
+
+    @PostMapping
+    public Passenger addPassenger(@RequestBody Passenger passenger) {
+        return passengerService.addPassenger(passenger);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deletePassenger(@PathVariable("id") int id) {
+        return passengerService.deletePassenger(id);
+    }
+
+    @GetMapping("/{id}")
+    public Passenger getPassengerById(@PathVariable int id) {
+        return passengerService.getPassengerById(id);
+    }
+
+    @GetMapping
+    public List<Passenger> getAllPassengers() {
+        return passengerService.getAllPassengers();
+    }
 }
